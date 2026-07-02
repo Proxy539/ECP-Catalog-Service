e15e09438534e75558fa938902a856a871d11c32
Proxy539 <roman.filiyutich@gmail.com>
2026-07-02 14:29:18 +0300

    init commit

---
+++ src/main/java/com/proxy/ecpcatalogservice/EcpCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.proxy.ecpcatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcpCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcpCatalogServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proxy/ecpcatalogservice/EcpCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proxy.ecpcatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcpCatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
