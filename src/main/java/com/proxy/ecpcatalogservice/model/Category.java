93c689959d629b42a35f77bbc82b7ff165624dde
Proxy539 <roman.filiyutich@gmail.com>
2026-07-23 20:40:43 +0300

    Add category entity (#6)

--- src/main/java/com/proxy/ecpcatalogservice/model/Category.java
+++ src/main/java/com/proxy/ecpcatalogservice/model/Category.java
@@ -1,5 +1,20 @@
 package com.proxy.ecpcatalogservice.model;
 
-public record Category (String id, String name, String description) {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public record Category (
+        @Id
+        @GeneratedValue
+        String id, 
+
+        @Column(nullable = false, length = 100)
+        String name,
+
+        @Column(length = 500)
+        String description) {
 
 }
